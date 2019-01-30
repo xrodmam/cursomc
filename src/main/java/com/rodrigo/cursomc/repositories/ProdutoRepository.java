8d1c05891a29d39cd2c110f84888204009fc8d31
xrodmam <machadomarins@gmail.com>
2019-01-30 00:17:00 -0200

    Produto e associação muitos para muitos

---
+++ src/main/java/com/rodrigo/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.rodrigo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.rodrigo.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {}
