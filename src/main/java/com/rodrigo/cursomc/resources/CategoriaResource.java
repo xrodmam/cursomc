1f96abaf6cd53e2e722d1c6ceae2f3c8941c1da3
xrodmam <machadomarins@gmail.com>
2019-02-09 23:50:12 -0200

    Alterações no endpoint /categoria/{id}: Tratamento de referência ciclica e retorno de objeto nulo.

--- src/main/java/com/rodrigo/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rodrigo/cursomc/resources/CategoriaResource.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.rodrigo.cursomc.domain.Categoria;
 import com.rodrigo.cursomc.services.CategoriaService;
 
@@ -17,9 +18,10 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<?> find(@PathVariable Integer id) {	
 		
-		Categoria obj = service.buscar(id);
+		Categoria obj;		
+		obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 		
 	}
--- src/main/java/com/rodrigo/cursomc/services/CategoriaService.java
+++ src/main/java/com/rodrigo/cursomc/services/CategoriaService.java
@@ -1,10 +1,15 @@
 package com.rodrigo.cursomc.services;
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.rodrigo.cursomc.domain.Categoria;
 import com.rodrigo.cursomc.repositories.CategoriaRepository;
+import com.rodrigo.cursomc.services.exceptions.ObjectNotFoundException;
+
+
 
 @Service
 public class CategoriaService {
@@ -12,10 +17,11 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria buscar(Integer id) throws ObjectNotFoundException  {
 		
-		Optional<Categoria> obj = repo.findById(id);		
-		return obj.orElse(null);
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		
 	}
 }
